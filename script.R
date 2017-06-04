6dd175e0f9a731d4e14197f1bf00f39871e47527
hmorris3 <hmorrison914@gmail.com>
2017-06-04 09:58:33 -0400

    edited R script

--- script.R
+++ script.R
@@ -3,8 +3,12 @@
  #Load data concerning devs with least hours
  dataLeast <-read.csv("Documents/DataChallenge/DataUsing/workedLeast.csv")
 #Histogram for count of Debug events
-hist(dataMost$Debug.COUNT...) 
-hist(dataLeast$Debug.COUNT...)
+hist(dataMost$Debug.COUNT...,
+     main = "Debug Usage in Most Active Developers",
+     xlab = "Total Debugger Use") 
+hist(dataLeast$Debug.COUNT...,
+     main = "Debug Usage in Least Active Developers",
+     xlab = "Total Debugger Use")
 #Correlation between copying and pasting: .957
 cor(dataMost$Copy.COUNT, dataMost$Paste.COUNT)
 #Correlation between copying and pasting: .985
@@ -18,10 +22,16 @@ cor(dataLeast$Debug.COUNT...,dataLeast$Paste.COUNT)
 #Debug.Total = Debug/Total, proportion of event over total events
 #Histogram for proportion of Debug events over total events 
 #in most active programmers
-hist(dataMost$Debug.Total)
+hist(dataMost$Debug.Total,
+     main = "Debugging as a Proportion of All Events 
+     in Most Active Developers",
+     xlab = "Debugging Event Total/Total All Events")
 #Histogram for proportion of Debug events over total events 
 #in least active programmers
-hist(dataLeast$Debug.Total)
+hist(dataLeast$Debug.Total,
+     main = "Debugging as a Proportion of All Events 
+     in Least Active Developers",
+     xlab = "Debugging Event Total/Total All Events")
 #Make a table with only the median values for the proportions 
 #of events;
 #the first row is the most active developers, 
