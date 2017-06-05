74cb75c16ffd3750f8049238f3c6f0e0a4530b0a
unknown <jareinha@ncsu.edu>
2017-06-05 09:40:42 -0400

    edited script

--- script.R
+++ script.R
@@ -49,7 +49,7 @@ combinedTable <- rbind(debugDataL,debugDataM)
 #barplot(rownames(combinedTable), columns(combinedTable), main="Debug commands used per person", xlab="Number of debugs", col=c("red","darkblue"), beside=TRUE)
 
 #Separated bar graphs, because this is getting ridiculous
-barplot(dataMost$Debug.COUNT..., dataMost$Hours.COUNT, main="Debug commands used per person", ylab="Number of debugs", xlab="hours",col=c("red","darkblue"), legend=dataMost$userId, beside=TRUE)
-barplot(dataLeast$Debug.COUNT..., dataLeast$Hours.COUNT, main="Debug commands used per person", ylab="Number of debugs", xlab="hours", col=c("red", "darkblue"), legend=dataLeast$userId)
+barplot(dataMost$Paste.COUNT, dataMost$Hours.COUNT, main="Paste commands used per person", ylab="Number of paste commands", xlab="hours",col=c("red","darkblue"), legend=dataMost$userId, beside=TRUE)
+barplot(dataLeast$Paste.COUNT, dataLeast$Hours.COUNT, main="Paste commands used per person", ylab="Number of paste commands", xlab="hours", col=c("red", "darkblue"), legend=dataLeast$userId)
 plot(debugDataM)
 plot(debugDataL)
